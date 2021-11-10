4cc1d7def142c9f2d5a629c43ddb1e991f6f813f
Reuel Dave Cardines <rdcardines@gmail.com>
2021-11-10 13:53:37 +0800

    Final Djangogirls Web

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'djangogirls.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
